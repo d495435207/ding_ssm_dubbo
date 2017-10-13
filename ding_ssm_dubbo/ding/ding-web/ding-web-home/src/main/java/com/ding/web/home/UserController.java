363c2ba8706f6321dc0de9e40612eb33e5ed879f
丁亮华 <dinglh@jinxiang.com>
2017-10-13 18:28:01 +0800

    加了redis后

--- ding_ssm_dubbo/ding/ding-web/ding-web-home/src/main/java/com/ding/web/home/UserController.java
+++ ding_ssm_dubbo/ding/ding-web/ding-web-home/src/main/java/com/ding/web/home/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ding.biz.cache.GetCache;
 import com.ding.biz.dao.user.data.UserDO;
 import com.ding.biz.manager.user.UserManager;
 
@@ -25,11 +26,18 @@ public class UserController
 {
 	@Autowired
 	private UserManager userManager;
-	@Autowired
-	private TestService testService;
+	/*@Autowired
+	private TestService testService;*/
 	private final Log logger = LogFactory.getLog(getClass());
 	
-	
+	@GetCache(name="redis_user",value="userId")
+	@RequestMapping(value="/redis")
+    public String redis(Integer userId,Model model){
+		logger.info("试验redis0000000000000000");
+    	 UserDO userDO = userManager.get(userId);
+    	 model.addAttribute("user", userDO);
+    	return "user/user";
+    }
 	@RequestMapping(value="/user")
     public String user(Integer userId,Model model){
 		logger.info("00000000000000000000");
@@ -37,11 +45,11 @@ public class UserController
     	 model.addAttribute("user", userDO);
     	return "user/user";
     }
-	@RequestMapping(value="/test")
+	/*@RequestMapping(value="/test")
 	public String test(Integer id,Model model){
 		logger.info("-----------test-dubbo-");
 		TestDTO testDTO = testService.get(id);
 		model.addAttribute("test", testDTO);
 		return "user/test";
-	}
+	}*/
 }
